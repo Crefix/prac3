d1dff005ff2c80a7b42fcd8a412ca379a6d1a2f8
Akram <akramhoussam02@hotmail.com>
2020-05-21 00:54:44 +0200

    Implementacion de getTotal() y getCantidadIva()

---
+++ facturas/src/facturas/Implementar.java
@@ -0,0 +1,17 @@
+package Kraken;
+
+public void cambiarEstado(Estados estado){
+	}
+	public float getTotal(){
+		float numero;
+		return numero;
+		float total;
+		return total;
+	}
+	public float getCantidadIva(){
+		float numero;
+		return numero;
+		float IVA;
+		return IVA;
+	}
+}
